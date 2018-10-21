71cf1933fa5d1ebbef7eac7b15b02b47c8fcb6ce
ShulmanK <shulman.k.y@gmail.com>
2018-10-21 10:29:39 +0300

    add features remove task anf filter task

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const filterInput = document.getElementById('filter');
 //Working with clear tasks button
 const clearTaskBtn = document.querySelector(('.clear-tasks'));
 //Working with ul element for building li
-const tasksCollection = document.querySelector('.collection');
+const taskCollection = document.querySelector('.collection');
 
 //Further need to create a function will be load all events
 
@@ -16,19 +16,25 @@ loadEventListeners();
 
 function loadEventListeners () {
     form.addEventListener('submit', addTask);
+    taskCollection.addEventListener('click', removeTask);
+    clearTaskBtn.addEventListener('click', clearTasks);
+    filterInput.addEventListener('keyup', filterTask);
+
 }
 
 //Need to create the function for add tasks
 function addTask (e) {
     if (newTaskInput.value === "") {
-        alert('Add task please')
-    }
+        alert('Add task please');
+        return
+    } else {
+
    const li = document.createElement('li');
    li.className = 'collection-item';
 
    li.appendChild(document.createTextNode(newTaskInput.value));
 
-   tasksCollection.appendChild(li);
+   taskCollection.appendChild(li);
 
    const link = document.createElement('a');
    link.className = 'delete-item secondary-content';
@@ -40,7 +46,64 @@ function addTask (e) {
     newTaskInput.value = '';
 
     e.preventDefault();
+
+    }
 }
 
+//Create function removeTask
 
+function removeTask (e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear tasks
+
+//One way
+function clearTasks (e) {
+//     taskCollection.innerHTML = '';
+//     console.log(e);
+//
+//Other way/faster
+
+while(taskCollection.firstChild) {
+    taskCollection.removeChild(taskCollection.lastChild);
+}
+}
+
+// Filter task
+
+function filterTask (e) {
+    const text = e.target.value.toLowerCase();
+    console.log('Поле фильтра' + text);
+
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+        console.log(item.toLowerCase().indexOf(text));
+
+        // item.toLowerCase().indexOf(text);
+    });
+
+
+
+    // document.querySelectorAll('.delete-item').forEach(function(task){
+    //     alert(task);
+    //     }
+};
+
+    // console.log(text);
+
+// }
 // console.log(form, newTaskInput, filterInput, clearTaskBtn, task);
+
+// document.querySelectorAll('.collection-item').forEach(function(task){
+//     console.log(task.firstChild);
+// });
